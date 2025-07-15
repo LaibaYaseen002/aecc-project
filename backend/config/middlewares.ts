12914385b01d6245abd543262076dc5d995e0539
laibayaseen002@gmail.com <Laibayaseen002@gmail.com>
2025-07-15 18:04:04 +0500

    Add backend folder as regular files

---
+++ backend/config/middlewares.ts
@@ -0,0 +1,19 @@
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: ["*"], // or replace * with your frontend URL
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
---
+++ backend/types/generated/components.d.ts
@@ -0,0 +1,20 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface NavbarNavChild extends Struct.ComponentSchema {
+  collectionName: 'components_navbar_nav_children';
+  info: {
+    displayName: 'NavChild';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'navbar.nav-child': NavbarNavChild;
+    }
+  }
+}
